c5168d375b139d30a8a2c70501e386a28556d314
Nayan Prasad P k <nayanprasad096@gmail.com>
2023-05-12 22:33:17 +0530

    addes eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 >welcome</h1>
-    </div>
-  )
+export default function Home() 
+{
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>welcome</h1>
+        </div>
+    );
 }
