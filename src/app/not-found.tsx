e66e7ba0b344817d1f9f855f9befde2ebc297e5f
Nayan Prasad P k <nayanprasad096@gmail.com>
2023-05-12 23:51:27 +0530

    husky updated

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-function NotFound()
-{
+function NotFound() {
     return (
         <div>
             Not Found
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-export default function Home() 
-{
+export default function Home() {
     return (
         <div className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1>welcome</h1>
