fdeb61d6c9f261de9653924deb1b7ac1cacd3b2b
Nayan Prasad P k <nayanprasad096@gmail.com>
2023-05-14 01:51:53 +0530

    added auto refresh config next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, //
+    reactStrictMode: true,
+    reactRefresh: true,
     eslint: {
         dirs: ["src"],
 
--- src/app/test/error.js
+++ src/app/test/error.js
@@ -5,7 +5,7 @@ import React from "react";
 const Error = () => {
     return (
         <div className="text-amber-500 min-h-screen">
-            <p>error</p>
+            <p>error...!!!</p>
         </div>
     );
 };
