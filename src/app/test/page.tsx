e0b9a8e34ee12b74ba0e3fde5714255662b49acf
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-05-28 01:41:45 +0530

    removed build error

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -8,7 +8,7 @@ interface User {
 }
 
 async function getData(): Promise<User[]> {
-    const res = await fetch("https://jsonplaceholder.typicode.com/usersadsf");
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
     if (!res.ok) {
         throw new Error("Failed to fetch data");
     }
